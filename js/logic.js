9d6d730cf028b96e1cc5ce835b21ad5db133e208
Bryan Castleman <castlemaninc@gmail.com>
2017-12-06 11:50:28 -0800

    adding some placeholder images for project page, and fixed CSS for background image

---
+++ js/logic.js
@@ -0,0 +1,7 @@
+// when user clicks project thumbnail
+	// clicked thumbnail becomes 100% opacity 
+	// large image replaces previous image
+	// corresponding heading loads
+	// corresponding description loads 
+	
+
