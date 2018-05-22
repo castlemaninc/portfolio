c6552e921a9033f763ae817fffb0f407c3fe0dca
Bryan Castleman <castlemaninc@gmail.com>
2018-05-21 22:28:12 -0700

    adding new link for font-awesome library and a down chevron in front of hero image

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
+    console.log('clicked');
   // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
